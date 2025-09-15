d514eb2b00275b1af73e27cd042f0e0cfe3c13c7
ThanosMak <makedasathanasios@gmail.com>
2025-09-15 10:34:14 +0200

    Test

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace SayHello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHello("World");
+            Console.ReadLine();
+        }
+
+        static void SayHello(string name)
+        {
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}
